d0391bba55a765c07e540dedfff80d3c89244690
DhanishMA <danishma2099pi@gmail.com>
2023-11-21 02:32:06 -0800

    Adding project files

---
+++ MaxArena/Intermediate/Build/Win64/MaxArenaEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
@@ -0,0 +1,2 @@
+// Compiler: 14.29.30153
+#include "C:/Users/DhanishMA/Documents/Unreal Projects/MaxArena/Intermediate/Build/Win64/MaxArenaEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
---
+++ MaxArena/Source/MaxArena/MaxArena.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "MaxArena.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, MaxArena, "MaxArena" );
